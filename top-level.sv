c032079d2b46b1b3b3df6dcb309eb6ed65de1a17
Grant Haines <hainesg@oregonstate.edu>
2019-12-04 14:33:36 -0800

    Added VGA output structure
    Not final version, did not add any inputs to project aside from 50Mz clock. The modules used by the VGA module are in the vga/ folder.

--- top-level.sv
+++ top-level.sv
@@ -4,8 +4,39 @@
 
 */
 
-module top-level();
+module top-level
+		(input clock50MHz);
 
-
+	NesReader NesReader1 (
+		.dataYellow(),
+		.clock(),
+		.reset_n(),
+		.latchOrange(),
+		.clockRed(),
+		.up(),
+		.down(),
+		.left(),
+		.right(),
+		.start(),
+		.select(),
+		.a(),
+		.b()
+	);
+		
+	vgaOutput vgaOutput1 (
+		.clock50MHz(clock50MHz),
+		.inReset(),
+		.redSwitch1(),
+		.redSwitch2(),
+		.greenSwitch1(),
+		.greenSwitch2(),
+		.blueSwitch1(),
+		.blueSwitch2(),
+		.hsync(),
+		.vsync(),
+		.outRed(),
+		.outGreen(),
+		.outBlue()
+	);
 
 endmodule
