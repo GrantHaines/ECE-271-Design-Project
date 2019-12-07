2a94dd898f59b291d4a620ce61c1b3d7559302fd
Ben218Adams <50226835+Ben218Adams@users.noreply.github.com>
2019-12-06 17:50:18 -0800

    updated squarewave.sv
    renamed module too

--- SquareWave.sv
+++ SquareWave.sv
@@ -1,3 +1,40 @@
+module SquareWave(input logic clk,
+					input logic [9:0] data,
+					output logic q);
+					
+int compareNumber;
+int count;
+logic posFlag;
+
+always_ff @(negedge q)
+	begin
+		compareNumber = (data + 100)*(10/7);
+		if (count >= compareNumber)
+			posFlag = 1;
+		else 
+			posFlag = 0; 		// general syntax for resetting count outside of reg?
+	end
+		
+always_ff @(posedge clk)
+begin
+	if (posFlag == 1)
+		count<= 0;				//
+	else
+		count <= count +1;
+end
+
+always_comb
+	begin
+			if( count < compareNumber)				
+				q = (count > compareNumber/2); 	//assigns output duty cycle 50% with initial low
+			else 
+				q = 0;
+	end
+
+endmodule
+
+
+/*
 module periodTime(input logic clk,
 					input logic [9:0] data,
 					output logic q);
@@ -25,4 +62,4 @@ always_comb
 				q = 0;
 	end
 
-endmodule
+endmodule*/
